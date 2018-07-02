918edff6f143e8a90be870f9d683739c6799deb5
arun <arunkumar@365build.in>
2018-07-02 16:09:38 +0530

    change 2

--- analyze.py
+++ analyze.py
@@ -1,3 +1,29 @@
 import time
 import csv
 import pandas as pd
+import numpy as np
+import psycopg2 as pg
+
+
+
+def process_chunk(chunk):
+    answer=chunk.groupby('consignor_state').consginor_state.count()
+    return answer
+
+size=5000000
+
+start=time.time()
+connection=pg.connect("dbname=ewaybill_test user=postgres")
+
+i=0
+
+for data_chunk in pd.read_sql_query("select * from ewaybill", connection, chunksize=size):
+    if(i==0):
+        result=process_chunk(data_chunk)
+        print("Took %s seconds" %(time.time()-start))
+        i=i+1
+    else:
+        exit()
+    del(data_chunk)
+
+result.to_csv('/tmp/op.csv')
