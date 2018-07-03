3588a52afa2a24ece78416601b0bbbffcd5f2d43
arun <arunkumar@365build.in>
2018-07-03 10:43:35 +0530

    Chunking logic change

--- analyze-new.py
+++ analyze-new.py
@@ -4,13 +4,17 @@ import psycopg2 as pg
 import csv
 
 start=time.time()
-connection=pg.connect("dbname=ewaybill_test user=postgres")
+connection=pg.connect("dbname='ewaybill_test' user='postgres' port='5433'")
+
+def process_chunk(chunk):
+    answer=chunk.groupby('consignor_state').consignor_state.count()
+    return answer
 
 chunk_size=1000000
 offset=1000000
 
 while True:
-    df=pd.read_sql_query("SELECT * FROM MyTable limit %d offset %d order by ID" % (chunk_size,offset))
+    df=pd.read_sql_query("SELECT * FROM ewaybill limit %d offset %d order by ID" % (chunk_size,offset))
     if offset == 1000000:
         result=process_chunk(df)
     else:
@@ -18,7 +22,7 @@ while True:
     del(df)
     chunk_size+=chunk_size
     offset+=offset
-    if (offset>77200000):
+    if (offset>25000000):
         break
 
 result.to_csv('/tmp/op.csv')
